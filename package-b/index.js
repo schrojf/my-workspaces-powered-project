27a0280463d456d27610208d6ad40d3c3161fe22
Viliam Schrojf <viliam@schrojf.sk>
2023-06-04 23:48:42 +0200

    Add package scope

--- package-b/index.js
+++ package-b/index.js
@@ -1,4 +1,4 @@
-const sum = require('package-a');
+const sum = require('@my-workspaces-powered-project/package-a');
 
 module.exports = function printSum(a, b) {
   console.log('Hello from package-b: ' + sum(a, b));
