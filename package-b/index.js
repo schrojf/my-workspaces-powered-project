2dae33c6001c21d4630c03a3dabd89554c8a77c7
Viliam Schrojf <viliam@schrojf.sk>
2023-06-05 00:21:58 +0200

    Add external dependency to package-b

--- package-b/index.js
+++ package-b/index.js
@@ -1,6 +1,9 @@
+const isOdd = require('is-odd');
 const sum = require('@my-workspaces-powered-project/package-a');
 
 module.exports = function printSum(a, b) {
+  console.log('a ' + (isOdd(a) ? 'is' : 'is not') + ' odd');
+  console.log('b ' + (isOdd(b) ? 'is' : 'is not') + ' odd');
   console.log('Hello from package-b: ' + sum(a, b));
 };
 
