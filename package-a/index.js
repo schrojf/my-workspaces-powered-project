2b68213607c2dd8373cf73297d8a9860ee1daa16
Viliam Schrojf <viliam@schrojf.sk>
2023-06-05 00:27:58 +0200

    Add external dependency to package-a

--- package-a/index.js
+++ package-a/index.js
@@ -1,4 +1,8 @@
+const isNumber = require('is-number');
+
 module.exports = function sum(a, b) {
+  console.log('a ' + (isNumber(a) ? 'is' : 'is not') + ' number');
+  console.log('a ' + (isisNumberOdd(a) ? 'is' : 'is not') + ' number');
   return a + b;
 };
 
